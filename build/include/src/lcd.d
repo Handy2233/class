28be162dd201fbe723846d05f5f6f0638f784046
Handy-tomori <594364858@qq.com>
2026-06-20 20:45:45 +0800

    删除LCD相关的构建文件和主程序文件

--- build/include/src/lcd.d
+++
@@ -1,6 +0,0 @@
-build/include/src/lcd.o: include/src/lcd.c include/inc/lcd.h \
- include/inc/log.h
-
-include/inc/lcd.h:
-
-include/inc/log.h:
--- build/main.d
+++
@@ -1,7 +0,0 @@
-build/main.o: main.c GEC6818.h include/inc/lcd.h include/inc/log.h
-
-GEC6818.h:
-
-include/inc/lcd.h:
-
-include/inc/log.h:
